4e785729b80c91d2c07a0240788a1f215e7e1a98
soji717 <106127692+soji717@users.noreply.github.com>
2022-05-23 22:16:14 -0400

    first code ever

---
+++ index.js
@@ -0,0 +1,8 @@
+const person = {
+    firstName: "Justin",
+    lastName: "Ly",
+    age: 15,
+    buttHygeine: "poopy"
+}
+
+console.log(`${person.firstName} ${person.lastName} is ${person.age} years old and has a ${person.buttHygeine} butt!`);
